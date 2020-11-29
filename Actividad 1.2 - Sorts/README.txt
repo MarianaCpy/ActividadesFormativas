1d78596e33a838caf5fd9c735ffc80a538666a91
MarianaCpy <64471347+MarianaCpy@users.noreply.github.com>
2020-11-29 17:22:53 -0600

    Add files via upload

---
+++ Actividad 1.2 - Sorts/README.txt
@@ -0,0 +1,39 @@
+------ Ordena selección ------------
+
+Análsis de complejidad:
+
+El orden selección, ordena el arreglo recorriendolo repedidamente por medio de iteraciones, encontrando el menor elemento.
+
+La complejidad de este algoritmo de ordenamiento es de O(n^2) ya que el algoritmo mantiene dos arreglos y se hace uso de dos ciclos anidados.
+
+------ Ordena Burbuja ----------
+
+Análisis de complejidad:
+
+Este método de ordenamiento es un algoritmo relativamente simple, intercambia continuamente cada elemento hasta que encuentra su posición correcta.
+
+La complejidad de este algoritmo en el mejor caso, que es cuando el arreglo ya esta ordenado es de O(n), en el peor caso la complejidad es de O(n*n)
+
+------ Ordena Merge ---------
+
+Análisis de complejidad:
+
+El algoritmo para este método de ordenamiento consiste en ir dividiendo el arreglo a la mitad para hacer su respectivo orden.
+
+La complejidad es de O(nLogn) en los tres casos, ya que siempre divide el arreglo a la mitad y después de alinearlas las vuelve a unir.
+
+------- Búsqueda Sequencial------
+
+Análisis de complejidad:
+
+Este algoritmo de busqueda es una simple búsqueda linear, en donde simplemente se van analizando las entradas del arreglo para verificar si el elemento está o no.
+
+La complejidad de éste es de O(1) en el mejor caso y O(n) en el peor y caso promedio. esto porque es iterativo, se va checando uno por uno.
+
+------- Búsqueda Binaria--------
+
+Análisis de complejidad:
+
+Este algoritmo busca en un arreglo ordenado dividiendo repetidamente el intervalo de la búsqueda.
+
+La complejidad de este es de O(Logn), ya que por medio del intervalo se cubre todo el arreglo.		
---
+++ Actividad 1.2 - Sorts/sorts.h
@@ -0,0 +1,178 @@
+/*
+Mariana Castro Payns
+A01706038
+Act 1.2
+*/
+
+#ifndef SORTS_H_
+#define SORTS_H_
+
+#include <vector>
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class Sorts {
+	private:
+	void swap(vector<int>&, int, int);
+	void arrayMerge(vector<int>&, vector<int>&,vector<int>&);
+	public:
+  void ordenaSeleccion(vector<int>&);
+void ordenaBurbuja(vector<int>&);
+  void ordenaMerge(vector<int>&);
+  int busqSecuencial(const vector<int> &, int);
+  int busqBinaria(const vector<int> &, int);
+
+};
+
+
+template <class T>
+void Sorts<T>::swap(vector<int> &v, int i, int j) {
+	T sus = v[i];
+	v[i] = v[j];
+	v[j] = sus;
+}
+
+template <class T>
+void Sorts<T>::ordenaSeleccion(vector<int> &vect){
+	int band;
+	
+	for (int i = vect.size() - 1; i>0; i--){
+		band = 0;
+		for (int j=1; j<=i; j++){
+			if (vect[j]>vect[band]){
+				band=j;
+			}
+		}
+		
+		if (band !=i){
+			swap(vect,i,band);
+		}
+	}
+}
+
+void swap(int *xp, int *yp)  
+{  
+    int temp = *xp;  
+    *xp = *yp;  
+    *yp = temp;  
+}  
+  
+void selectionSort(int arr[], int n)  
+{  
+    int i, j, min_idx;  
+  
+    // One by one move boundary of unsorted subarray  
+    for (i = 0; i < n-1; i++)  
+    {  
+        // Find the minimum element in unsorted array  
+        min_idx = i;  
+        for (j = i+1; j < n; j++)  
+        if (arr[j] < arr[min_idx])  
+            min_idx = j;  
+  
+        // Swap the found minimum element with the first element  
+        swap(&arr[min_idx], &arr[i]);  
+    }  
+} 
+ 
+  
+/* Function to print an array */
+void printArray(int arr[], int size)  
+{  
+    int i;  
+    for (i=0; i < size; i++)  
+        cout << arr[i] << " ";  
+    cout << endl;  
+}  
+ 
+
+template <class T>
+void Sorts<T>::ordenaBurbuja(vector<int> &bubble){
+
+	for(int i = bubble.size() - 1; i>0; i--) {
+		for (int j = 0; j < i; j++) {
+			if (bubble[j] > bubble[j+1]){
+				swap(bubble, j , j + 1);
+			}
+		}
+}
+}	
+
+template <class T>
+void Sorts<T>::arrayMerge(vector<int> &inf, vector<int> &sup, vector<int> &b){
+  int i=0, j=0, k=0;
+	
+	while (j < inf.size() && k < sup.size()){
+		if (inf[j] < sup[k]){
+			b[i] = inf[j];
+			j++;
+		} else {
+			b[i]=sup[k];
+			k++;
+		}
+		i++;
+	}
+	while(j<inf.size()){
+    b[i]=inf[j];
+    j++; i++;
+	}
+	while(k<sup.size()){
+    b[i]=sup[k];
+    k++; i++;
+		}
+	}
+	
+
+template <class T>
+void Sorts<T>::ordenaMerge(vector<int> &b){
+  if(b.size() <= 1) return;
+
+  int m = b.size() / 2;
+  vector<int> left;
+  vector<int> right;
+
+  for(size_t j=0; j<m;j++)
+    left.push_back(b[j]);
+  for(size_t j=0; j<(b.size())-m; j++)
+    right.push_back(b[m+j]);
+
+  ordenaMerge(left);
+  ordenaMerge(right);
+  arrayMerge(left,right,b);
+}
+
+template <class T>
+int Sorts<T>::busqSecuencial(const vector<int> &b, int val){
+
+for (int i=0; i<b.size(); i++)
+  if (b[i]==val)
+    return i;
+  return -1;
+}
+
+template <class T>
+int Sorts<T>::busqBinaria(const vector<int> &bin, int val){
+
+int mit;
+int inf=0;
+int sup=bin.size() - 1;
+
+while(inf<sup){
+  mit = (inf+sup)/2;
+  if (val==bin[mit]){
+    return mit;
+  }
+  else if(val < bin[mit]){
+    sup=mit-1;
+  }
+  else if(bin[mit]<val){
+    inf=mit+1;
+  }
+}
+return inf;
+}
+
+#endif
